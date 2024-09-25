638e94046ddea397aee97fc65bb4141554cdd9e5
Ewan <ewankjones@gmail.com>
2024-09-25 19:01:27 +0000

    aes-encrypt work

--- aes-encrypt.py
+++ aes-encrypt.py
@@ -1,20 +1,44 @@
+
 import sys
 
-# total arguments
-n = len(sys.argv)
-print("Total arguments passed:", n)
+def usage():
+    print("aes-encrypt usage:")
+    print("\tkey=<a file name containing 128-bit key as a hex string>")
+    print("\tIV<a file name containing IV as a hex string>")
+    print("\tmode=ecb|cbc")
+    print("\tin=<the input file name>")
+    print("\tout=<the output file name>")
+    print("aes-encrypt examples:")
+    print("\taes-encrypt.py key=file_key IV=iv_file mode=cbc in=input_file out=out_file")
+    print("\taes-encrypt.py key=file_key mode=ecb in=input_file out=out_file")
 
-# Arguments passed
-print("\nName of Python script:", sys.argv[0])
 
-print("\nArguments passed:", end = " ")
-for i in range(1, n):
-    print(sys.argv[i], end = " ")
-    
-# Addition of numbers
-Sum = 0
-# Using argparse module
-for i in range(1, n):
-    Sum += int(sys.argv[i])
-    
-print("\n\nResult:", Sum)
+
+arg_len = len(sys.argv)
+
+if arg_len < 5:
+    usage()
+    exit(1)
+
+
+cmd_args = sys.argv
+key_filename = None
+iv_filename = None
+mode = None
+in_filename = None
+out_filename = None
+
+
+if "mode=cbc" in cmd_args and not any(arg[:3] == "IV=" for arg in cmd_args):
+    usage()
+    exit(1)
+
+
+
+
+
+
+for i in range(1, arg_len):
+    print(sys.argv[i], end=" ")
+
+print(arg_len)
