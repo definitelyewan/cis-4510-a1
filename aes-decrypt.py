7e6828de2325ed5d39c7c0a07ea0695ae0fd8d11
Ewan <ewankjones@gmail.com>
2024-09-26 10:00:25 -0400

    2e

--- aes-decrypt.py
+++ aes-decrypt.py
@@ -8,9 +8,10 @@ def usage():
     print("aes-decrypt usage:")
     print("\tkey=<a file name containing 128-bit key as a hex string>")
     print("\tiv<a file name containing IV as a hex string>")
-    print("\tmode=ecb|cbc")
+    print("\tmode=ecb|cbc|gcm")
     print("\tin=<the input file name>")
     print("\tout=<the output file name>")
+    print("\tgcm_args=<the name of a file, which contains a parameter for authentication>")
     print("aes-decrypt examples:")
     print("\taes-decrypt.py key=file_key iv=iv_file mode=cbc in=input_file out=out_file")
     print("\taes-decrypt.py key=file_key mode=ecb in=input_file out=out_file")
@@ -44,6 +45,10 @@ if "mode=cbc" in cmd_args and not any(arg[:3] == "iv=" for arg in cmd_args):
     usage()
     exit(1)
 
+if "mode=gcm" in cmd_args and not any(arg[:3] == "iv=" for arg in cmd_args) and not any(arg[:9] == "gcm_args=" for arg in cmd_args):
+    usage()
+    exit(1)
+
 for i in range(1, len(sys.argv)):
     if cmd_args[i][:4] == "key=":
         key_hex = read_file_in_bytes(cmd_args[i][4:])
@@ -57,9 +62,14 @@ for i in range(1, len(sys.argv)):
             usage()
             exit(1)
         iv = bytes.fromhex(iv_hex.decode().strip())
+    elif cmd_args[i][:9] == "gcm_args=":
+        gcm_args = read_file_in_bytes(cmd_args[i][9:])
+        if gcm_args is None:
+            usage()
+            exit(1)
     elif cmd_args[i][:5] == "mode=":
         mode = cmd_args[i][5:]
-        if mode != "ecb" and mode != "cbc":
+        if mode != "ecb" and mode != "cbc" and mode != "gcm":
             usage()
             exit(1)
     elif cmd_args[i][:3] == "in=":
@@ -73,22 +83,28 @@ for i in range(1, len(sys.argv)):
             usage()
             exit(1)
 
-if mode not in ["ecb", "cbc"]:
+if mode not in ["ecb", "cbc", "gcm"]:
     usage()
     exit(1)
 
 #decrypt
+cipher = None
+encrypted_data = None
+
 if mode == "ecb":
     cipher = Cipher(algorithms.AES(key), modes.ECB())
 elif mode == "cbc":
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+elif mode == "gcm":
+    cipher = Cipher(algorithms.AES(key), modes.GCM(iv, gcm_args, 16))
 
 decryptor = cipher.decryptor()
 decrypted_data = decryptor.update(in_data) + decryptor.finalize()
 
-#unpad
-unpadder = padding.PKCS7(128).unpadder()
-unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+if mode in ["ecb", "cbc"]:
+    unpadder = padding.PKCS7(128).unpadder()
+    unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+    write_file_in_bytes(out_filename, unpadded_data)
 
-#write
-write_file_in_bytes(out_filename, unpadded_data)
+elif mode == "gcm":
+    write_file_in_bytes(out_filename, decrypted_data)
--- aes-encrypt.py
+++ aes-encrypt.py
@@ -8,9 +8,10 @@ def usage():
     print("aes-encrypt usage:")
     print("\tkey=<a file name containing 128-bit key as a hex string>")
     print("\tiv<a file name containing IV as a hex string>")
-    print("\tmode=ecb|cbc")
+    print("\tmode=ecb|cbc|gcm")
     print("\tin=<the input file name>")
     print("\tout=<the output file name>")
+    print("\tgcm_args=<the output filename for a gcm tag>")
     print("aes-encrypt examples:")
     print("\taes-encrypt.py key=file_key iv=iv_file mode=cbc in=input_file out=out_file")
     print("\taes-encrypt.py key=file_key mode=ecb in=input_file out=out_file")
@@ -38,10 +39,15 @@ cmd_args = sys.argv
 key = None
 iv = None
 mode = None
+gcm_args = None
 in_data = None
 out_filename = None
 
 if "mode=cbc" in cmd_args and not any(arg[:3] == "iv=" for arg in cmd_args):
+    usage()
+    exit(1)
+
+if "mode=gcm" in cmd_args and not any(arg[:3] == "iv=" for arg in cmd_args) and not any(arg[:9] == "gcm_args=" for arg in cmd_args):
     usage()
     exit(1)
 
@@ -58,9 +64,14 @@ for i in range(1, len(sys.argv)):
             usage()
             exit(1)
         iv = bytes.fromhex(iv_hex.decode().strip())
+    elif cmd_args[i][:9] == "gcm_args=":
+        gcm_args = cmd_args[i][9:]
+        if gcm_args is None:
+            usage()
+            exit(1)
     elif cmd_args[i][:5] == "mode=":
         mode = cmd_args[i][5:]
-        if mode != "ecb" and mode != "cbc":
+        if mode != "ecb" and mode != "cbc" and mode != "gcm":
             usage()
             exit(1)
     elif cmd_args[i][:3] == "in=":
@@ -74,22 +85,32 @@ for i in range(1, len(sys.argv)):
             usage()
             exit(1)
 
-if mode not in ["ecb", "cbc"]:
+if mode not in ["ecb", "cbc", "gcm"]:
     usage()
     exit(1)
 
-#pad
-padder = padding.PKCS7(algorithms.AES.block_size).padder()
-padded_data = padder.update(in_data) + padder.finalize()
 
 #encrypt
+cipher = None
+encrypted_data = None
+
 if mode == "ecb":
     cipher = Cipher(algorithms.AES(key), modes.ECB())
 elif mode == "cbc":
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+elif mode == "gcm":
+    cipher = Cipher(algorithms.AES(key), modes.GCM(iv, None, 16))
 
 encryptor = cipher.encryptor()
-encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+
+if mode in ["ecb", "cbc"]:
+    padder = padding.PKCS7(algorithms.AES.block_size).padder()
+    padded_data = padder.update(in_data) + padder.finalize()
+    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+
+elif mode == "gcm":
+    encrypted_data = encryptor.update(in_data) + encryptor.finalize()
+    write_file_in_bytes(gcm_args, encryptor.tag)
 
 #write
 write_file_in_bytes(out_filename, encrypted_data)
